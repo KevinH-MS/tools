8592b7bf501b8b17232b6050b1cacaa0095a075a
Kevin Halverson <kevinh@microsoft.com>
2016-11-18 15:14:23 -0800

    Utilities for printing the MVID of all managed assemblies...
    ...under the current directory.

---
+++ MVID/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+class Program
+{
+    static void Main()
+    {
+        foreach (var pattern in new[] { "*.dll", "*.exe" })
+        {
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, pattern, SearchOption.AllDirectories))
+            {
+                using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (var peReader = new PEReader(stream))
+                {
+                    try
+                    {
+                        Console.Write(file);
+                        Console.Write("\t");
+                        var reader = peReader.GetMetadataReader();
+                        Console.WriteLine(reader.GetGuid(reader.GetModuleDefinition().Mvid));
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Not a managed assembly...
+                    }
+                }
+            }
+        }
+    }
+}
